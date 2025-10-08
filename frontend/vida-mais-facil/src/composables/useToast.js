884bc61b67f8d73176019cb7e954c6b36ba9229e
Eduardo Ribeiro de Lima <limareduardo@gmail.com>
2025-10-08 11:26:44 -0300

    feat: adiciona o sistema de toasts

---
+++ frontend/vida-mais-facil/src/composables/useToast.js
@@ -0,0 +1,23 @@
+import Toastify from 'toastify-js'
+
+export function useToast() {
+  function pushToast(level, message, duration = 5000) {
+    const className = `bg-${level}-subtle border border-${level}-subtle rounded-2 `
+                     + `text-${level}-emphasis`
+
+    console.log(className)
+
+
+    Toastify({
+      text: message,
+      duration: duration,
+      close: false,
+      gravity: "bottom",
+      className: className,
+    }).showToast()
+  }
+  
+  return {
+    pushToast
+  }
+}
